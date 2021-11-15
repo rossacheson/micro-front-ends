a3d0e63ee5f16987212381ce63ddbd13a240fb1a
Ross Acheson <ross.acheson@gmail.com>
2021-11-15 17:54:53 -0500

    Add comment to trigger deploy-container action/workflow

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,6 +1,7 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
 
+// simple component to mount the remote app
 export default () => {
     const ref = useRef(null);
 
