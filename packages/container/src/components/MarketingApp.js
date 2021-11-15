84f131cc419e8dc119d5af93bcd4a8f7f0ced2f8
Ross Acheson <ross.acheson@gmail.com>
2021-11-15 18:02:52 -0500

    Change comment to trigger pipeline

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,7 +1,7 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
 
-// simple component to mount the remote app
+// simple component to mount the marketing remote app
 export default () => {
     const ref = useRef(null);
 
