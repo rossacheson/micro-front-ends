a2c0b23a307afa58b2772fa2cb053eef58312a07
Ross Acheson <ross.acheson@gmail.com>
2021-11-15 10:40:42 -0500

    Add container react app

---
+++ packages/container/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/styles';
+
+export default () => {
+    return (
+    <h1>Hi there!</h1>
+    )
+}
