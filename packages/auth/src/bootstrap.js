ec30d6bcbcaf35b770600c61a9eb54f49ff4ca6a
Ross Acheson <ross.acheson@gmail.com>
2021-11-16 18:35:20 -0500

    Add auth webpack configs

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,40 @@
+import React  from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+import App from './App'
+
+// Mount function
+const mount  = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate){
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(
+        <App history={history}/>,
+        el
+    );
+
+    return {
+        onParentNavigate({ pathname: nextPathname }) {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
+}
+
+// If we are in dev and isolation
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector("#_auth-dev-root");
+
+    if (devRoot) {
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
+    }
+}
+
+// export for container to use
+export { mount };
