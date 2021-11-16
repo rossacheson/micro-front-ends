5b59f577d31f7b26379309302441452ed4446a59
Ross Acheson <ross.acheson@gmail.com>
2021-11-16 18:24:13 -0500

    Routing comuncation up and down

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,13 +1,27 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 // simple component to mount the marketing remote app
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(
+            ref.current,
+            {
+                onNavigate: ({ pathname: nextPathname }) => {
+                    const { pathname } = history.location;
+
+                    if (pathname !== nextPathname) {
+                        history.push(nextPathname);
+                    }
+                }
+            });
+
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref} />
 };
